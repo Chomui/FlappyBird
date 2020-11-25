62697900071454ac27ade31ab5f3a4b597f4aaa6
= <artem.uvarenko@chisw.com>
2020-11-25 17:06:03 +0200

    Add bird; Minor Refactoring

--- app/src/main/java/com/chiswstudy/flappybird/FlappyBird.kt
+++ app/src/main/java/com/chiswstudy/flappybird/FlappyBird.kt
@@ -1,13 +1,17 @@
 package com.chiswstudy.flappybird
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewTreeObserver
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.DecelerateInterpolator
 import android.view.animation.LinearInterpolator
-
 
 class FlappyBird @JvmOverloads constructor(
     context: Context,
@@ -15,59 +19,107 @@ class FlappyBird @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
-    val tree: Bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.tree)
-    val reversedTree: Bitmap = tree.createFlippedBitmap(xFlip = false, yFlip = true)
-
-    /*val screenWidth: Float = (context.getSystemService(Context.WINDOW_SERVICE) as WindowManager)
-        .defaultDisplay
-        .let {
-            val point = Point()
-            it.getSize(point)
-            point.x.toFloat()
+    private val tree: Bitmap =
+        BitmapFactory.decodeResource(context.resources, R.drawable.tree).let {
+            Bitmap.createScaledBitmap(
+                it,
+                it.width / 2,
+                it.height / 2,
+                false
+            )
         }
-    val screenHeight: Float = (context.getSystemService(Context.WINDOW_SERVICE) as WindowManager)
-        .defaultDisplay
-        .let {
-            val point = Point()
-            it.getSize(point)
-            point.y.toFloat()
-        }*/
-    var viewWidth = 0F
-    var viewHeight = 0F
 
-    val paint = Paint().apply {
+    private val reversedTree: Bitmap = tree.createFlippedBitmap(xFlip = false, yFlip = true)
+    private val bird: Bitmap = Bitmap.createScaledBitmap(
+        BitmapFactory.decodeResource(context.resources, R.drawable.bird),
+        200,
+        200,
+        false
+    )
+
+    private var viewWidth = 0F
+    private var viewHeight = 0F
+
+    private val paint = Paint().apply {
         isAntiAlias = true
     }
+    private val paintBird = Paint()
 
-    var left1 = 0F
-    var left2 = 0F
-    var left3 = 0F
-    val treeSpace = 0.8F
+    private var left1: Float = 0F
+    private var left2: Float = 0F
+    private var left3: Float = 0F
+    private val treeSpace: Float = 0.8F
+    private var birdTop: Float = 0F
+    private var birdUpAnimator: ValueAnimator? = null
+
+    private lateinit var birdFallAnimator: ValueAnimator
 
     init {
-        viewTreeObserver.addOnGlobalLayoutListener(object :
-            ViewTreeObserver.OnGlobalLayoutListener {
-            override fun onGlobalLayout() {
-                viewHeight = height.toFloat()
-                viewWidth = width.toFloat()
-                viewTreeObserver.removeOnGlobalLayoutListener(this)
-
-                moveTrees()
-            }
-        })
+        prepare()
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        canvas?.drawBitmap(tree, left1, viewHeight - (tree.height / 2), paint)
-        canvas?.drawBitmap(reversedTree, left1, 0F - (tree.height / 2), paint)
+        drawTrees(canvas)
 
-        canvas?.drawBitmap(tree, left2, viewHeight - (tree.height / 2), paint)
-        canvas?.drawBitmap(reversedTree, left2, 0F - (tree.height / 2), paint)
+        drawBird(canvas)
+    }
 
-        canvas?.drawBitmap(tree, left3, viewHeight - (tree.height / 2), paint)
-        canvas?.drawBitmap(reversedTree, left3, 0F - (tree.height / 2), paint)
+    override fun onTouchEvent(event: MotionEvent?): Boolean = when (event?.action) {
+        MotionEvent.ACTION_DOWN -> {
+            performClick()
+            true
+        }
+        else -> false
+    }
+
+    override fun performClick(): Boolean {
+        super.performClick()
+        startFlyAnimation()
+        return true
+    }
+
+    private fun prepare() {
+        viewTreeObserver.addOnGlobalLayoutListener(object :
+            ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                viewTreeObserver.removeOnGlobalLayoutListener(this)
+
+                // Get View Size
+                viewHeight = height.toFloat()
+                viewWidth = width.toFloat()
+
+                // Bird start position
+                birdTop = (viewHeight / 2) - (bird.height / 2)
+
+                moveTrees()
+
+                // Create animator for bird to fall until user clicks
+                birdFallAnimator = createBirdFallAnimator()
+                birdFallAnimator.start()
+            }
+        })
+    }
+
+    private fun drawTrees(canvas: Canvas?) {
+        canvas?.drawBitmap(tree, left1, viewHeight - tree.height, paint)
+        canvas?.drawBitmap(reversedTree, left1, 0F, paint)
+
+        canvas?.drawBitmap(tree, left2, viewHeight - tree.height, paint)
+        canvas?.drawBitmap(reversedTree, left2, 0F, paint)
+
+        canvas?.drawBitmap(tree, left3, viewHeight - tree.height, paint)
+        canvas?.drawBitmap(reversedTree, left3, 0F, paint)
+    }
+
+    private fun drawBird(canvas: Canvas?) {
+        canvas?.drawBitmap(
+            bird,
+            (viewWidth / 2) - (bird.width / 2),
+            birdTop,
+            paintBird
+        )
     }
 
     private fun moveTrees() {
@@ -79,25 +131,75 @@ class FlappyBird @JvmOverloads constructor(
             addUpdateListener { it ->
                 val value = it.animatedValue as Float
 
-                if (value <= 0 && value > -treeSpace) {
-                    left1 = viewWidth * value
-                    left2 = viewWidth * (value + treeSpace)
-                    left3 = viewWidth * (value + 2 * treeSpace)
-                } else if (value <= -treeSpace && value > -2 * treeSpace) {
-                    left1 = viewWidth * (value + 3 * treeSpace)
-                    left2 = viewWidth * (value + treeSpace)
-                    left3 = viewWidth * (value + 2 * treeSpace)
-                } else if (value <= -2 * treeSpace && value > -3 * treeSpace) {
-                    left1 = viewWidth * (value + 3 * treeSpace)
-                    left2 = viewWidth * (value + 4 * treeSpace)
-                    left3 = viewWidth * (value + 2 * treeSpace)
-                }
+                changeTreesCoordinateX(value)
 
                 invalidate()
             }
             start()
         }
+
     }
+
+    private fun changeTreesCoordinateX(value: Float) {
+        if (value <= 0 && value > -treeSpace) {
+            left1 = viewWidth * value
+            left2 = viewWidth * (value + treeSpace)
+            left3 = viewWidth * (value + 2 * treeSpace)
+        } else if (value <= -treeSpace && value > -2 * treeSpace) {
+            left1 = viewWidth * (value + 3 * treeSpace)
+            left2 = viewWidth * (value + treeSpace)
+            left3 = viewWidth * (value + 2 * treeSpace)
+        } else if (value <= -2 * treeSpace && value > -3 * treeSpace) {
+            left1 = viewWidth * (value + 3 * treeSpace)
+            left2 = viewWidth * (value + 4 * treeSpace)
+            left3 = viewWidth * (value + 2 * treeSpace)
+        }
+    }
+
+    private fun startFlyAnimation() {
+        // Cancel previous animations
+        birdFallAnimator.cancel()
+        birdUpAnimator?.cancel()
+
+        // Start Fly
+        birdUpAnimator = createBirdUpAnimator()
+        birdUpAnimator?.start()
+    }
+
+    private fun createBirdFallAnimator(): ValueAnimator =
+        ValueAnimator.ofFloat(birdTop, viewHeight).apply {
+            duration = 800
+            interpolator = AccelerateInterpolator()
+            addUpdateListener {
+                val value = it.animatedValue as Float
+                birdTop = if (value <= viewHeight - bird.height) {
+                    value
+                } else {
+                    viewHeight - bird.height
+                }
+                invalidate()
+            }
+        }
+
+    private fun createBirdUpAnimator(): ValueAnimator =
+        ValueAnimator.ofFloat(birdTop, birdTop - 100).apply {
+            duration = 300
+            interpolator = DecelerateInterpolator()
+            addUpdateListener {
+                val value = it.animatedValue as Float
+                birdTop = if (value >= 0) {
+                    value
+                } else {
+                    0F
+                }
+            }
+            addListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    birdFallAnimator = createBirdFallAnimator()
+                    birdFallAnimator.start()
+                }
+            })
+        }
 
     private fun Bitmap.createFlippedBitmap(xFlip: Boolean, yFlip: Boolean): Bitmap {
         val matrix = Matrix()
